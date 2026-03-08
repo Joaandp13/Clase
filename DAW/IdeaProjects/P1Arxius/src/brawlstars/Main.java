8f99352c491facaac42659870122075a9b75f7c9
Joaandp13 <joaandp117@gmail.com>
2026-03-08 22:58:43 +0100

    adsd

--- DAW/IdeaProjects/P1Arxius/src/brawlstars/Main.java
+++ DAW/IdeaProjects/P1Arxius/src/brawlstars/Main.java
@@ -3,28 +3,45 @@ package brawlstars;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Main{
-
-
+    public static Scanner sc = new Scanner(System.in);
+    static boolean running = true;
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        while(true) {
+
+        while(running) {
             System.out.println(
-                    "1.Esborrar carpeta" +
-                            "\n2.Crear carpeta" +
-                            "\n3.Copiar estructura carpeta" +
-                            "\n4.Crear fitxers" +
-                            "\n5.Moure/Renombrar fitxer" +
-                            "\n6.Elegir linies d'un fitxer" +
-                            "\n7.Guardar contingut de Brawlers.txt" +
-                            "\n8.Comptar paraules a Brawlers.txt" +
-                            "\n9.Mostrar recompenses d'un jugador" +
-                            "\n10.Generar recompenses especials" +
-                            "\n0.Acabar" +
-                            "\nSelecciona una opció (0-10): "
+                    """
+                            
+                            --------MENU--------\
+                            
+                            1.Esborrar carpeta\
+                            
+                            2.Crear carpeta\
+                            
+                            3.Copiar estructura carpeta\
+                            
+                            4.Crear fitxers\
+                            
+                            5.Moure/Renombrar fitxer\
+                            
+                            6.Elegir linies d'un fitxer\
+                            
+                            7.Guardar contingut de Brawlers.txt\
+                            
+                            8.Comptar paraules a Brawlers.txt\
+                            
+                            9.Mostrar recompenses d'un jugador\
+                            
+                            10.Generar recompenses especials\
+                            
+                            0.Acabar\
+                            
+                            Selecciona una opció (0-10):\s"""
             );
             int opcio = sc.nextInt();
+
             try {
                 switch (opcio) {
                     case 1:
@@ -34,36 +51,126 @@ public class Main{
                         func2();
                         break;
                     case 3:
+                        func3();
+                        break;
+                    case 4:
+                        sc.nextLine();
+                        System.out.println("Introdueix la ruta a la que vols crear els arxius: ");
+                        File ruta = new File(sc.nextLine());
+                        File[] arxius = func4A();
+
+                        func4B(ruta.getPath(), arxius);
+                        break;
+                    case 0:
+                        System.out.println("""
+            ████████╗ █████╗ ███╗  ██╗ ██████╗ █████╗ ███╗  ██╗████████╗
+               ██╔══╝██╔══██╗████╗ ██║██╔════╝██╔══██╗████╗ ██║╚══██╔══╝
+               ██║   ███████║██╔██╗██║██║     ███████║██╔██╗██║   ██║   
+               ██║   ██╔══██║██║╚████║██║     ██╔══██║██║╚████║   ██║   
+               ██║   ██║  ██║██║ ╚███║╚██████╗██║  ██║██║ ╚███║   ██║   
+               ╚═╝   ╚═╝  ╚═╝╚═╝  ╚══╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚══╝   ╚═╝    
+                                                              
+            """);
+                        running = false;
+                        for(int i= 0;i<3;++i){
+                            Thread.sleep(600);
+                            System.out.println(".");
+                        }
+                        break;
+
                 }
             }catch(Exception e){System.out.println("Error: "+e);}
+            sc.nextLine();
         }
     }
+
     public static void func1()throws Exception{
         File brawlers = new File("./Brawlers");
         if(brawlers.exists()){
             FileUtils.deleteDirectory(brawlers);
             System.out.println("Directori esborrat correctament.");
         }
-        else{throw new Exception("No existeix el directori. ");}
+        else{throw new Exception("No existeix el directori Brawlers. ");}
     }
-    public static void func2()throws Exception{
-        File Tank = new File("./Brawlers/Tank");
-        File Legendary = new File (Tank+"/Legendary");
-        File Epic = new File (Tank+"/Epic");
-        if(Tank.exists()){
-            System.out.println("La carpeta Tank ja existeix. ");
-            if(FileUtils.directoryContains(Tank,Legendary)){
-                boolean legflag = true;
+
+
+    public static void func2()throws Exception {
+
+        boolean creardir = true;
+        while (creardir) {
+            System.out.print("Introdueix la ruta del directori que vols crear:  ");
+
+            File directori = new File(sc.next());
+
+            if (!directori.exists()) {
+                FileUtils.forceMkdir(directori);
+                System.out.println("Directori creat correctament.");
+            } else {
+                throw new Exception("El directori ja existeix.");
             }
-            else if(FileUtils.directoryContains(Tank,Epic)){
-                boolean epicflag = true;
+            System.out.println("Vols crear un altre directori? S/N");
+            if (sc.next().equalsIgnoreCase("N")) {
+                creardir = false;
             }
-            else {FileUtils.forceMkdir(Tank);
-            F}
         }
-
-
     }
 
 
-}
+
+
+    public static void func3()throws Exception{
+
+        System.out.println("Introdueix el directori que vols copiar: ");
+        File directoriorigen = new File(sc.next());
+        if (!directoriorigen.exists()) {
+            throw new Exception("El directori no existeix.");
+        }
+        System.out.println("Introdueix on vols copiar el directori Ex: ./Exemple/[Nom Nou] : ");
+        File directoridesti = new File(sc.next());
+        if (directoridesti.exists()) {
+            throw new Exception("El directori "+ directoridesti+  " ja existeix. ");
+        }
+        FileUtils.copyDirectory(directoriorigen, directoridesti);
+        System.out.println("Directori copiat correctament.");
+    }
+    public static File[] func4A() throws Exception {
+        ArrayList<File> filelist = new ArrayList<>();
+        boolean input = true;
+
+        while (input) {
+            System.out.print("Introdueix el nom del fitxer per crear (buit per cancelar): ");
+            String nom = sc.nextLine();
+            if (nom.isEmpty()) {
+                input = false;
+            }
+
+            else if (filelist.contains(new File(nom))) {
+                throw new Exception("Fitxer ja introduït.");
+            }
+
+            else {
+                filelist.add(new File(nom));
+            }
+        }
+        return filelist.toArray(new File[0]);
+    }
+
+
+    public static void func4B(String Ruta, File[] noms)throws Exception{
+        for (File nom : noms) {
+            File a = new File(Ruta, nom.getName());
+
+            if (a.exists()) {
+                throw new Exception("El fitxer " + nom.getName() + " ja existeix.");
+            }
+
+            FileUtils.forceMkdir(a);
+            System.out.println("Fitxer creat: " + a.getPath());
+        }
+    }
+    }
+
+
+
+
+
