7aa0259d22030334afebfcf04435e24c877298ac
Joan <joaandp117@gmail.com>
2026-04-02 00:42:07 +0200

    clase

---
+++ DAW/IdeaProjects/Llapisos i daus/src/Llapis.java
@@ -0,0 +1,11 @@
+public class Llapis implements Comparable<Llapis>{
+    /**
+     * Implementem la interfície Comparable, ja que la classe Llapis farà servir un mètode compareTo implementat
+     * Comparable és una interfície genèrica:
+     * public interface Comparable<T> {
+     *     int compareTo(T o);
+     * }
+     * però nosaltres volem comparar Llapis, per això fem Comparable<Llapis>
+     * */
+
+}
