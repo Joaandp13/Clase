25ac16ee05188ce81b2e2eb8373865db0a7ad701
Joan <joaandp117@gmail.com>
2026-04-13 12:34:48 +0200

    fix nested git repo

--- DAW/Programacion/WebStorm/Programacion bloc1/1/index.js
+++ DAW/Programacion/WebStorm/Programacion bloc1/1/index.js
@@ -1,1 +1,27 @@
 console.log('Happy developing ✨')
+const hola = document.getElementById('hola');
+const img = document.getElementById('imagen');
+hola.innerHTML = hola.innerText;
+
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const textoOriginal= hola.innerText;
+let bandera= true;
+btn1.addEventListener('click', () => {
+
+    if (bandera) {
+        hola.innerHTML = "<b>Adios</b>"
+        img.style.display = "block";
+
+        bandera = false;
+    } else {
+        hola.innerHTML = textoOriginal;
+        img.style.display = "none";
+        bandera =true;
+    }
+})
+
+
+
+
+
